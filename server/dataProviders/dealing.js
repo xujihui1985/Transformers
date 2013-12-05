51891da247e0bb39a512688db57ba68e7a09351d
Scott.L <scott.l@xg>
2013-12-05 19:19:59 +0800

    apply `mongoHandler` on `dealing.getById`

--- server/dataProviders/dealing.js
+++ server/dataProviders/dealing.js
@@ -23,30 +23,19 @@ exports.getAll = function () {
 			def.resolve(items);
 		});
 	});
-//	
-//    MongoClient.connect("mongodb://localhost:27017/transformer", function (err, db) {
-//        if (err) {
-//            deferred.reject(err);
-//        }
-//        var collection = db.collection('dealing');
-//        collection.find().toArray(function (err, items) {
-//            deferred.resolve(items);
-//        });
-//    });
+	
     return deferred.promise;
 };
 
 exports.getById = function (id) {
     var deferred = when.defer();
-    MongoClient.connect("mongodb://localhost:27017/transformer", function (err, db) {
-        if (err) {
-            deferred.reject(err);
-        }
-        var collection = db.collection('dealing');
-        collection.findOne({_id: id}, function (err, item) {
-            deferred.resolve(item);
+	
+	mongoHandler.collection(deferred, 'dealing', function(def, col) {
+		col.findOne({_id: id}, function (err, item) {
+            def.resolve(item);
         });
-    });
+	});
+	
     return deferred.promise;
 };
 
