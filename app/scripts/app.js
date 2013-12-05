a65de32f3634f252a3a2893d57640b30858a3ded
York Chen <qgmzwrym@gmail.com>
2013-12-05 16:09:33 +0800

    add deals page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-angular.module('transformersApp', [])
+angular.module('transformersApp', ["ngGrid"])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
+      .when('/', {templateUrl: 'views/main.html',controller: 'MainCtrl'})
+	  .when('/deal', {templateUrl: 'views/deal.html', controller: 'DealCtrl'})
       .otherwise({
         redirectTo: '/'
       });
