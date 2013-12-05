a698f331f81e3dddb6f2c446d45b0b5f2e0d6b03
Scott.L <scott.l@xg>
2013-12-05 17:07:10 +0800

    extract the manipulation of mongo collection into a helper function
    extract to function named `mongoHelper`
    rewrite `dealing.getAll()`

--- server/dataProviders/dealing.js
+++ server/dataProviders/dealing.js
@@ -2,17 +2,35 @@ var MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
     when = require('when');
 
+var mongoHelper = function (def, collectionId, collectionHandler) {
+	MongoClient.connect("mongodb://localhost:27017/transformer", function (err, db) {
+		if (err) {def.reject(err);}
+		
+		var collection = db.collection(collectionId);
+		
+		collectionHandler(def, collection);
+	});
+	return def.promise;
+};
+
 exports.getAll = function () {
     var deferred = when.defer();
-    MongoClient.connect("mongodb://localhost:27017/transformer", function (err, db) {
-        if (err) {
-            deferred.reject(err);
-        }
-        var collection = db.collection('dealing');
-        collection.find().toArray(function (err, items) {
-            deferred.resolve(items);
-        });
-    });
+	
+	mongoHelper(deferred, 'dealing', function(def, col) {
+		col.find().toArray(function(err, items) {
+			def.resolve(items);
+		});
+	});
+//	
+//    MongoClient.connect("mongodb://localhost:27017/transformer", function (err, db) {
+//        if (err) {
+//            deferred.reject(err);
+//        }
+//        var collection = db.collection('dealing');
+//        collection.find().toArray(function (err, items) {
+//            deferred.resolve(items);
+//        });
+//    });
     return deferred.promise;
 };
 
@@ -28,5 +46,17 @@ exports.getById = function (id) {
         });
     });
     return deferred.promise;
-}
+};
+
+exports.saveItem = function (id, item) {
+	var deferred = when.defer();
+	
+	mongoHelper(deferred, 'dealing', function(def, col) {
+		col.find().toArray(function(err, items) {
+			def.resolve(items);
+		});
+	});
+	
+	return deferred.promise;
+};
 
